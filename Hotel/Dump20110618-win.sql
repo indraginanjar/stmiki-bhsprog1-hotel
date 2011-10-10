7c0b415aa2629bf7f01c6f1cecf0cc3dca514bc3
Indra Ginanjar <indra@the-pooh.(none)>
2011-10-10 16:19:30 +0700

    Project Netbeans yang dikirim sugiarto

---
+++ Hotel/Dump20110618-win.sql
@@ -0,0 +1,104 @@
+CREATE DATABASE  IF NOT EXISTS `dbakademik` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `dbakademik`;
+-- MySQL dump 10.13  Distrib 5.1.54, for debian-linux-gnu (i686)
+--
+-- Host: 127.0.0.1    Database: dbakademik
+-- ------------------------------------------------------
+-- Server version	5.1.54-1ubuntu4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `jabatan`
+--
+
+DROP TABLE IF EXISTS `jabatan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jabatan` (
+  `id` tinyint(2) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jabatan`
+--
+
+LOCK TABLES `jabatan` WRITE;
+/*!40000 ALTER TABLE `jabatan` DISABLE KEYS */;
+INSERT INTO `jabatan` VALUES (1,'Kepala Yayasan STMIK INDONESIA'),(2,'Wakil Ketua Yayasan STMIK INODNESIA'),(3,'Ketua Jurusan'),(4,'Wakil Ketua Jurusan'),(7,'Kepala Mandor'),(6,'Raja Pulau Dewi');
+/*!40000 ALTER TABLE `jabatan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `matakuliah`
+--
+
+DROP TABLE IF EXISTS `matakuliah`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `matakuliah` (
+  `kode` varchar(12) NOT NULL,
+  `matakuliah` varchar(40) NOT NULL,
+  `sks` tinyint(2) NOT NULL,
+  `semester` tinyint(2) NOT NULL,
+  PRIMARY KEY (`kode`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `matakuliah`
+--
+
+LOCK TABLES `matakuliah` WRITE;
+/*!40000 ALTER TABLE `matakuliah` DISABLE KEYS */;
+INSERT INTO `matakuliah` VALUES ('KDE-101','Bahasa Pemprograman 1',3,2);
+/*!40000 ALTER TABLE `matakuliah` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+DROP TABLE IF EXISTS `mahasiswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mahasiswa` (
+  `no_bp` varchar(12) NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `prodi` varchar(2) NOT NULL,
+  PRIMARY KEY (`no_bp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+LOCK TABLES `mahasiswa` WRITE;
+/*!40000 ALTER TABLE `mahasiswa` DISABLE KEYS */;
+INSERT INTO `mahasiswa` VALUES ('101100180','Indra Ginanjar','1'),('101100200','Kirana Tamasya','21');
+/*!40000 ALTER TABLE `mahasiswa` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-06-18  8:47:51
